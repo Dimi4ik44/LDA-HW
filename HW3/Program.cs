d44a73f3cddd917940de5d1ef5d8ea40518d2789
Dimi4ik44 <Dimi4ik44@gmail.com>
2021-07-28 16:43:10 +0300

    Add project files.

---
+++ HW3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HW3
+{
+    class Program
+    {
+         static string[] errorMessageList = new string[5];
+        
+        static void Main(string[] args)
+        {
+            errorMessageList[0] = "Incorect input";
+            int arraySize = readNumber();
+            Console.WriteLine(arraySize);
+        }
+
+        static int readNumber()
+        {
+            int number;
+            while(!(Int32.TryParse(Console.ReadLine(), out number)))
+            {
+                errorMessageSender(errorMessageList[0]);
+            }
+            return number;
+        }
+        static void errorMessageSender(string error)
+        {
+            Console.WriteLine($"Error: {error}");
+        }
+    }
+}
