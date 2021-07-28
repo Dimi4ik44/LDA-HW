a54429a1dec603cd6ec4a7ac286e85cda09893f4
Dimi4ik44 <Dimi4ik44@gmail.com>
2021-07-28 17:01:33 +0300

    init empty error messages

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -8,7 +8,13 @@ namespace HW3
         
         static void Main(string[] args)
         {
+            //init error messages
             errorMessageList[0] = "Incorect input";
+            errorMessageList[1] = "Empty";
+            errorMessageList[2] = "Empty";
+            errorMessageList[3] = "Empty";
+            errorMessageList[4] = "Empty";
+            //code
             int arraySize = readNumber();
             Console.WriteLine(arraySize);
         }
@@ -18,11 +24,11 @@ namespace HW3
             int number;
             while(!(Int32.TryParse(Console.ReadLine(), out number)))
             {
-                errorMessageSender(errorMessageList[0]);
+                sendErrorMessage(errorMessageList[0]);
             }
             return number;
         }
-        static void errorMessageSender(string error)
+        static void sendErrorMessage(string error)
         {
             Console.WriteLine($"Error: {error}");
         }
