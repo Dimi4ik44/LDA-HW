f791a4d20896266abc79bff30ea2c1efc4d45be5
Dimi4ik44 <Dimi4ik44@gmail.com>
2021-07-28 21:16:28 +0300

    All task compleated

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -17,6 +17,10 @@ namespace HW3
             //code
             int arraySize = readNumber();
             int[,] myArray = new int[arraySize, arraySize];
+            // arrayPrint(arrayFillRandom(myArray));
+            // arrayPrint(arrayFillSquareOutline(myArray));
+            // arrayPrint(arrayFillTypeX(myArray));
+            arrayPrint(arrayFillPiramid(myArray));
 
             static int readNumber()
             {
@@ -28,19 +32,123 @@ namespace HW3
                 }
                 return number;
             }
-
-            static void arrayFillSquareOutline(int[,] array)
+            static int[,] arrayFillRandom(int[,] array)
             {
-
+                int lenghtDimen0, lenghtDimen1, max=int.MinValue, min=int.MaxValue, holder;
+                double middle , sum = 0;
+                lenghtDimen0 = array.GetLength(0);
+                lenghtDimen1 = array.GetLength(1);
+                Random rnd = new Random();
+                for (int i = 0; i < lenghtDimen0; i++)
+                {
+                    for (int k = 0; k < lenghtDimen1; k++)
+                    {
+                        holder = rnd.Next(0, 10);
+                        if (holder < min) min = holder;
+                        if (holder > max) max = holder;
+                        sum += holder;
+                        array[i, k] = holder;
+                    }
+                }
+                middle = sum / array.Length;
+                Console.WriteLine($"Min: {min}, Max: {max}, Middle: {middle}");
+                return array;
             }
-            static void arrayFillPiramid(int[,] array)
+            static int[,] arrayFillSquareOutline(int[,] array)
             {
-
+                int lenghtDimen0, lenghtDimen1;
+                lenghtDimen0 = array.GetLength(0);
+                lenghtDimen1 = array.GetLength(1);
+                for (int i = 0; i < lenghtDimen0; i++)
+                {
+                    for (int k = 0; k < lenghtDimen1; k++)
+                    {
+                        if((i == 0 || k == 0) || (i == lenghtDimen0 - 1 || k == lenghtDimen1 - 1))
+                        {
+                            array[i, k] = 1;
+                        }
+                        else
+                        {
+                            array[i, k] = 0;
+                        }
+                    }
+                }
+                return array;
             }
-            static void arrayFillTypeX(int[,] array)
+            static int[,] arrayFillPiramid(int[,] array)
             {
+                int lenghtDimen0, lenghtDimen1;
+                lenghtDimen0 = array.GetLength(0);
+                lenghtDimen1 = array.GetLength(1);
+                int i = 0, k = 0;
+                int counter = 1;
 
+                for (; i <= lenghtDimen0; i++)
+                {
+                    if (i<lenghtDimen0/2)
+                    {
+                        for (k = 0 + i; k < lenghtDimen1 - i; k++)
+                        {
+                            array[i, k] = counter++;
+                        }
+                        counter = 1;
+                    }
+                    else
+                    {
+                        for (int r = lenghtDimen1 - i; r < i; r++)
+                        {
+                            array[i - 1, r] = counter++;
+                        }
+                        counter = 1;
+                    }
+
+                    //array[i, 0 + i] = 1;
+                    //array[i, (lenghtDimen1 - 1) - i] = 1;
+                }
+                return array;
             }
+            static int[,] arrayFillPiramidV2(int[,] array)
+            {
+                int lenghtDimen0, lenghtDimen1;
+                lenghtDimen0 = array.GetLength(0);
+                lenghtDimen1 = array.GetLength(1);
+                
+                return array;
+            }
+            static int[,] arrayFillTypeX(int[,] array)
+            {
+                int lenghtDimen0, lenghtDimen1;
+                lenghtDimen0 = array.GetLength(0);
+                lenghtDimen1 = array.GetLength(1);
+                for (int i = 0; i < lenghtDimen0; i++)
+                {
+                    array[i, 0+i] = 1;
+                    array[i, (lenghtDimen1-1)-i] = 1;
+                }
+                return array;
+            }
+            static void arrayPrint(int[,] array)
+            {
+                int lenghtDimen0, lenghtDimen1;
+                lenghtDimen0 = array.GetLength(0);
+                lenghtDimen1 = array.GetLength(1);
+                string sep = "";
+                for (int i = 0; i < lenghtDimen1*6+1; i++)
+                {
+                    sep += "-";
+                }
+                for (int i = 0; i < lenghtDimen0; i++)
+                {
+                    Console.WriteLine(sep);
+                    for (int k = 0; k < lenghtDimen1; k++)
+                    {
+                        Console.Write($"|  {array[i, k]}  ");
+                    }
+                    Console.Write("|\n");
+                }
+                Console.WriteLine(sep);
+            }
+            //error sender
             static void sendErrorMessage(string error)
             {
                 Console.WriteLine($"Error: {error}");
