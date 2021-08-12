61cb73d4ef31df2bf8e71469d809733c0e7d544a
Dmutro <dimi4ik44@gmail.com>
2021-08-12 18:48:48 +0300

    init base variables

--- Assasin_Screed_Minigame/Program.cs
+++ Assasin_Screed_Minigame/Program.cs
@@ -4,9 +4,27 @@ namespace Assasin_Screed_Minigame
 {
     class Program
     {
+        //orlog
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Enemy
+            int enemyLives = 15;
+            int enemyVictimToken = 0;
+            int enemyDiceReroll = 3;
+            int[] enemyDice;
+            int[] enemySelectedDice;
+
+            //Player
+            int playerLives = 15;
+            int playerVictimToken = 0;
+            int playerDiceReroll = 3;
+            int[] playerDice;
+            int[] playerSelectedDice;
+
+            //general
+            int cubesCount = 6;
+            int coin = 0; //select the first move 1 - Player, 2 - Enemy
+
         }
     }
 }
