60d5a0dc88861ff5dd53e0260ae5e77f237c82d5
Dmutro <dimi4ik44@gmail.com>
2021-08-12 18:25:09 +0300

    Assasin Screed minigame work started

---
+++ Assasin_Screed_Minigame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assasin_Screed_Minigame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
