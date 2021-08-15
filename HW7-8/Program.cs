3223d975a9d3f1eb0a4efebb650f4dc0ce36bde8
Dmutro <dimi4ik44@gmail.com>
2021-08-15 19:28:00 +0300

    Start HW7-8

---
+++ HW7-8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW7_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
