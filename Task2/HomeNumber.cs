619dc9d3798906a9d229c4ad74cd0f2812f643d4
Dmutro <dimi4ik44@gmail.com>
2021-09-06 19:13:05 +0300

    -

--- Task2/HomeNumber.cs
+++ Task2/HomeNumber.cs
@@ -6,6 +6,10 @@ namespace Task2
 {
     class HomeNumber : PhoneNumber
     {
-
+        public bool CreateNumber(int number)
+        {
+            Number = number.ToString();         
+            return true;
+        }
     }
 }
